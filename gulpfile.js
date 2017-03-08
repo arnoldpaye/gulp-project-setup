c99351fe020361786edf7befeef01624bd123aea
Arnold Paye <arnoldpayet@gmail.com>
2017-03-08 08:58:13 -0400

    add build-css task.

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,23 @@
 var gulp = require('gulp');
 var gutil = require('gulp-util');
 var jshint = require('gulp-jshint');
+var sass = require('gulp-sass');
 
 // Default task that only log a message
 gulp.task('default', function() {
     return gutil.log('Gulp is running...');
 });
 
+// Jshint task
 gulp.task('jshint', function() {
     return gulp.src('src/js/**/*.js')
         .pipe(jshint())
         .pipe(jshint.reporter('jshint-stylish'));
+});
+
+// Build css task
+gulp.task('build-css', function() {
+    return gulp.src('src/scss/**/*.scss')
+        .pipe(sass())
+        .pipe(gulp.dest('build/css/main.css'));
 });
